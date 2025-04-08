f22f07f5b757aa5d3db1222d83825788557d5f82
zinkland <2261150574@qq.com>
2025-04-08 13:14:03 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -29,7 +29,7 @@ if [ ! -f "com.bilibili.AzurLane.apk" ]; then
 
     # eg: wget "your download link" -O "your packge name.apk" -q
     #if you want to patch .xapk, change the suffix here to wget "your download link" -O "your packge name.xapk" -q
-    wget "https://gc.s.migames.com/gdownload/api/c/download?c=app&v=download&package=com.bilibili.blhx.mi&channel=meng_1439_352_android" -O com.bilibili.AzurLane.apk 
+    wget "https://gc.s.migames.com/gdownload/api/c/download?c=app&v=download&package=com.bilibili.blhx.mi&channel=meng_1439_352_android" -O com.bilibili.AzurLane.apk -q
     
     
     
